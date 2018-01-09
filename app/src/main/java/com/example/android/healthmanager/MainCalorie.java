44a2fe7470d13c67878f42ffa81883ebb89c33e2
terra9 <avinash.r.kanaujiya@gmail.com>
2018-01-09 20:37:25 +0530

    First commit

---
+++ app/src/main/java/com/example/android/healthmanager/MainCalorie.java
@@ -0,0 +1,13 @@
+package com.example.android.healthmanager;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainCalorie extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_calorie);
+    }
+}
