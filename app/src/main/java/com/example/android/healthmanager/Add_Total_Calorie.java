dcd38a78a548a7325f1343958668708b2b8d3b9f
terra9 <avinash.r.kanaujiya@gmail.com>
2018-01-11 19:08:33 +0530

    update 11/01/2018

--- app/src/main/java/com/example/android/healthmanager/Add_Total_Calorie.java
+++ app/src/main/java/com/example/android/healthmanager/Add_Total_Calorie.java
@@ -5,24 +5,20 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class Add_Total_Calorie extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
-    public void open_calorie_counter(View v){
-        Intent i= new Intent(MainActivity.this,MainCalorie.class);
-        startActivity(i);
+        setContentView(R.layout.activity_add__total__calorie);
     }
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu options from the res/menu/menu_catalog.xml file.
         // This adds menu items to the app bar.
-        getMenuInflater().inflate(R.menu.maincalorie_menu, menu);
+
+        getMenuInflater().inflate(R.menu.addtotalcalorie_menu, menu);
         return true;
     }
     @Override
@@ -31,12 +27,12 @@ public class MainActivity extends AppCompatActivity {
         switch (item.getItemId()) {
             // Respond to a click on the "Insert dummy data" menu option
             case R.id.action_insert_dummy_data:
-                Intent home= new Intent(MainActivity.this,MainCalorie.class);
+                Intent home= new Intent(Add_Total_Calorie.this,MainActivity.class);
                 startActivity(home);
                 return true;
             // Respond to a click on the "Delete all entries" menu option
             case R.id.action_delete_all_entries:
-                Intent Login= new Intent(MainActivity.this,LoginActivity.class);
+                Intent Login= new Intent(Add_Total_Calorie.this,LoginActivity.class);
                 startActivity(Login);
                 return true;
         }
--- app/src/main/java/com/example/android/healthmanager/MainCalorie.java
+++ app/src/main/java/com/example/android/healthmanager/MainCalorie.java
@@ -1,7 +1,14 @@
 package com.example.android.healthmanager;
 
+import android.app.ActionBar;
+import android.content.Intent;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
 
 public class MainCalorie extends AppCompatActivity {
 
@@ -9,5 +16,52 @@ public class MainCalorie extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_calorie);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent addfood = new Intent(MainCalorie.this, add_food.class);
+                startActivity(addfood);
+            }
+        });
+
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu options from the res/menu/menu_catalog.xml file.
+        // This adds menu items to the app bar.
+
+        getMenuInflater().inflate(R.menu.maincalorie_menu, menu);
+        return true;
+    }
+    public void open_add_totalcalorie(View v) {
+        Intent addtotalCalorie = new Intent(MainCalorie.this, Add_Total_Calorie.class);
+        startActivity(addtotalCalorie);
+    }
+    public void open_suggestions(View v) {
+        Intent suggest = new Intent(MainCalorie.this, suggestions.class);
+        startActivity(suggest);
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // User clicked on a menu option in the app bar overflow menu
+        switch (item.getItemId()) {
+            // Respond to a click on the "Insert dummy data" menu option
+            case R.id.action_insert_dummy_data:
+                Intent home= new Intent(MainCalorie.this,MainActivity.class);
+                startActivity(home);
+                return true;
+            // Respond to a click on the "Delete all entries" menu option
+            case R.id.action_delete_all_entries:
+                Intent Login= new Intent(MainCalorie.this,LoginActivity.class);
+                startActivity(Login);
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+
+
 }
